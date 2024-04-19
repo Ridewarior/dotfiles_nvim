9ee949dc8037d6795969860906794b55d92c6af4
AMagallon <amagallon2@outlook.com>
2024-04-19 19:53:40 -0400

    Added mini plugins

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -23,4 +23,63 @@ return {
 
   -- Auto 'shiftwidth' and 'expandwidth'
   { 'tpope/vim-sleuth' },
+
+  -- Mini plugins that bring lots of good stuff :)
+  -- Better text objects
+  {
+    'echasnovski/mini.ai',
+    version = false,
+    keys = {
+      { 'a', mode = { 'x', 'o' } },
+      { 'i', mode = { 'x', 'o' } },
+    },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      init = function()
+        -- no need to load the plugin, since we only use the queries from it
+        require('lazy.core.loader').disable_rtp_plugin 'nvim-treesitter-textobjects'
+      end,
+    },
+    otps = function()
+      local ai = require 'mini.ai'
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+            i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+          }, {}),
+          f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
+          c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }, {})
+        },
+      }
+    end,
+    config = function(_, opts)
+      local ai = require 'mini.ai'
+      ai.setup(opts)
+    end,
+  },
+
+  -- Allows you to easily remove buffers
+  {
+    'echasnovski/mini.bufremove',
+    keys = {
+      { '<leader>bd', function() require('mini.bufremove').delete(0, false) end, desc = 'Delete Buffer' },
+      { '<leader>bD', function() require('mini.bufremove').delete(0, true) end,  desc = 'Delete Buffer (Force)' },
+    }
+  },
+
+  -- Surround Actions
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    opts = {},
+  },
+
+  -- Tabs
+  {
+    'echasnovski/mini.tabline',
+    version = false,
+    opts = {},
+  }
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,26 @@
+local swap_next, swap_prev = (function()
+  local swap_objects = {
+    p = "@parameter.inner",
+    f = "@function.outer",
+    c = "@class.outer",
+  }
+
+  local n, p = {}, {}
+  for key, obj in pairs(swap_objects) do
+    n[string.format("<leader>cx%s", key)] = obj
+    p[string.format("<leader>cX%s", key)] = obj
+  end
+
+  return n, p
+end)()
+
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   event = 'BufReadPost',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
   opts = {
     ensure_installed = {
       "bash",
@@ -40,6 +59,57 @@ return {
     endwise = {
       enable = true
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = 'gnn',
+        node_incremental = 'grn',
+        scope_incremental = 'grc',
+        node_decremental = 'grm',
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to text objects, similar to targets.vim
+        keymaps = {
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = swap_next,
+        swap_previous = swap_prev,
+      },
+    },
+    matchup = {
+      enable = true,
+    }
   },
   config = function(_, opts)
     -- Prefer git instead of curl to improve connectivity in some envs
