b008503a828812f8f3ba7eb3ef11212e9e182c27
AMagallon <amagallon2@outlook.com>
2024-04-21 23:40:30 -0400

    debugging improvements

--- lua/plugins/debugging/dap.lua
+++ lua/plugins/debugging/dap.lua
@@ -1,10 +1,11 @@
 local M = {}
 
 function M.configure()
-  local dap = require "dap"
-  local dapui = require "dapui"
+  local dap = require 'dap'
+  local dapui = require 'dapui'
+  local widgets = require 'dap.ui.widgets'
 
-  require("mason-nvim-dap").setup {
+  require('mason-nvim-dap').setup {
     -- makes bet effort to setup various debuggers
     -- with reasonable debug config
     automatic_installation = true,
@@ -13,25 +14,77 @@ function M.configure()
     -- see mason-nvim-dap readme for more info
     handlers = {},
     ensure_installed = {
-      "node2",
-      "firefox",
-      "chrome",
-      "js",
+      'node2',
+      'firefox',
+      'chrome',
+      'js',
     },
   }
 
-  local map = function(keys, func, desc)
-    vim.keymap.set("n", keys, func, { desc = "Debug: " .. desc })
+  local map = function(mode, keys, func, desc)
+    vim.keymap.set(mode, keys, func, { desc = 'Debug: ' .. desc })
   end
 
-  map("<F5>", dap.continue, "Start/Continue")
-  map("<F11>", dap.step_into, "Step Into")
-  map("<F10>", dap.step_over, "Step Over")
-  map("<S-F11>", dap.step_out, "Step Out")
-  map("<leader>b", dap.toggle_breakpoint, "Toggle Breakpoint")
-  map("<leader>B", function()
-    dap.set_breakpoint(vim.fn.input "Breakpoint condition: ")
-  end, "Set Breakpoint")
+  map('n', '<leader>dR', function()
+    dap.run_to_cursor()
+  end, 'Run to Cursor')
+  map('n', '<leader>dE', function()
+    dapui.eval(vim.fn.input '[Expression] > ')
+  end, 'Evaluate Input')
+  map('n', '<leader>dC', function()
+    dap.set_breakpoint(vim.fn.input '[Condition] > ')
+  end, 'Conditonal Breakpoint')
+  map('n', '<leader>dU', function()
+    dapui.toggle()
+  end, 'Toggle UI')
+  map('n', '<leader>db', function()
+    dap.step_back()
+  end, 'Step Back')
+  map('n', '<leader>dc', function()
+    dap.continue()
+  end, 'Start/Continue')
+  map('n', '<leader>dd', function()
+    dap.disconnect()
+  end, 'Disconnect')
+  map('n', '<leader>de', function()
+    dapui.eval()
+  end, 'Evaluate')
+  map('v', '<leader>de', function()
+    dapui.eval()
+  end, 'Evaluate')
+  map('n', '<leader>dg', function()
+    dap.session()
+  end, 'Get Session')
+  map('n', '<leader>dh', function()
+    widgets.hover()
+  end, 'Hover Variables')
+  map('n', '<leader>dS', function()
+    widgets.scopes()
+  end, 'Scopes')
+  map('n', '<leader>di', function()
+    dap.step_into()
+  end, 'Step Into')
+  map('n', '<leader>do', function()
+    dap.step_over()
+  end, 'Step Over')
+  map('n', '<leader>dp', function()
+    dap.pause.toggle()
+  end, 'Pause')
+  map('n', '<leader>dq', function()
+    dap.close()
+  end, 'Quite')
+  map('n', '<leader>dr', function()
+    dap.repl.toggle()
+  end, 'Toggle REPL')
+  map('n', '<leader>dt', function()
+    dap.toggle_breakpoint()
+  end, 'Toggle Breakpoint')
+  map('n', '<leader>dx', function()
+    dap.terminate()
+  end, 'Terminate')
+  map('n', '<leader>du', function()
+    dap.step_out()
+  end, 'Step Out')
 
   dapui.setup {
     icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
@@ -52,11 +105,13 @@ function M.configure()
 
   -- Toggle to see last session result.
   -- This is in case the session dies due unhandled exceptions
-  map('<F7>', dapui.toggle, 'See last session result.')
+  map('n', function()
+    dapui.toggle()
+  end, 'See last session results')
 
-  dap.listeners.after.event_initialized["dapui_config"] = dapui.open()
-  dap.listeners.before.event_terminated["dapui_config"] = dapui.close()
-  dap.listeners.before.event_exited["dapui_config"] = dapui.close()
+  dap.listeners.after.event_initialized['dapui_config'] = dapui.open()
+  dap.listeners.before.event_terminated['dapui_config'] = dapui.close()
+  dap.listeners.before.event_exited['dapui_config'] = dapui.close()
 end
 
 return M
--- lua/plugins/debugging/init.lua
+++ lua/plugins/debugging/init.lua
@@ -1,21 +1,21 @@
 local dap = require 'plugins.debugging.dap'
 return {
   {
-    "mfussenegger/nvim-dap",
-    event = "BufReadPre",
+    'mfussenegger/nvim-dap',
+    event = 'BufReadPre',
     dependencies = {
       -- Creates debugger UI
-      "rcarriga/nvim-dap-ui",
-      "nvim-neotest/nvim-nio",
-      "theHamsta/nvim-dap-virtual-text",
-      "nvim-telescope/telescope-dap.nvim",
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-telescope/telescope-dap.nvim',
 
       -- Installs debug adapters for you
-      "williamboman/mason.nvim",
-      "jay-babu/mason-nvim-dap.nvim",
+      'williamboman/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
 
       -- Allows you to debug any lua code nvim is currently using
-      "jbyuki/one-small-step-for-vimkind",
+      'jbyuki/one-small-step-for-vimkind',
     },
     config = dap.configure,
   },
