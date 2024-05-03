efbc8da98fa56ec1b688a7939ae309303bcfd193
AMagallon <amagallon2@outlook.com>
2024-05-03 09:19:55 -0400

    Base configs rewrite

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
-require 'config'
+require 'config.options'
+require 'config.lazy'
+
+vim.api.nvim_create_autocmd('user', {
+  group = vim.api.nvim_create_augroup('myconfig', { clear = true }),
+  pattern = 'VeryLazy',
+  callback = function()
+    require 'config.keymaps'
+    require 'config.autocmds'
+  end,
+})
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,56 +1,113 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("custom_group_" .. name, { clear = true })
+end
+
 -- check `:h vim.highlight.on_yank()`
-local hightlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking text",
-  group = hightlight_group,
+  group = augroup("YankHighLight"),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 
--- Check if a file reload is needed (e.g. file has changed from outside source)
-vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" })
-
--- Go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPre", {
-  pattern = "*",
+-- Check if we need to reload the file when it's changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("checktime"),
   callback = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "<buffer>",
-      once = true,
-      callback = function()
-        vim.cmd [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
-      end,
-    })
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
   end,
 })
 
--- Stop commenting on new line
-vim.api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
 
--- Easly close sub-menus
+-- Go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+-- vim.api.nvim_create_autocmd("BufReadPre", {
+--   pattern = "*",
+--   callback = function()
+--     vim.api.nvim_create_autocmd("FileType", {
+--       pattern = "<buffer>",
+--       once = true,
+--       callback = function()
+--         vim.cmd [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
+--       end,
+--     })
+--   end,
+-- })
+
+-- Quick close frequent filetypes
 vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
   pattern = {
-    "OverseerForm",
-    "OverseerList",
-    "floggraph",
-    "fugitive",
-    "git",
+    -- "OverseerForm",
+    -- "OverseerList",
+    -- "floggraph",
+    -- "fugitive",
+    -- "git",
     "help",
     "lspinfo",
-    "man",
+    "notify",
+    "checkhealth",
+    -- "man",
     "neotest-output",
     "neotest-summary",
+    "neotest-output-panel",
     "qf",
     "query",
     "spectre_panel",
     "startuptime",
-    "toggleterm",
-    "vim",
+    "PlenaryTestPopup",
+    -- "toggleterm",
+    -- "vim",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
     vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("man_unlisted"),
+  pattern = { "man" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
   end,
 })
 
@@ -71,5 +128,17 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
       vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
       vim.wo.cursorline = false
     end
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
